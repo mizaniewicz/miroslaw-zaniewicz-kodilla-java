4d3c6ead6d12f792fc15544dc37f3f90f9c80ad4
mizaniewicz <mizaniewicz@gmail.com>
2017-07-18 10:48:18 +0200

    junit, czyli pierwsze testy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -8,7 +8,7 @@ import com.kodilla.testing.user.SimpleUser;
  */
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        SimpleUser simpleUser = new SimpleUser("theForumUser", "John Smith");
 
         String result = simpleUser.getUsername();
 
