394464198eff61cb9841a294a824750c4cc00b9a
mizaniewicz <mizaniewicz@gmail.com>
2017-07-18 10:01:33 +0200

    first commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing;
+
+/**
+ * Created by mizan on 18.07.2017.
+ */
+public class TestingMain {
+    public static void main(String[] args) {
+        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    }
+}
