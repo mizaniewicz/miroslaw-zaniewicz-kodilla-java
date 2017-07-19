f25811a1a4d03eaa55343a5dad451562e604d34e
mizaniewicz <mizaniewicz@gmail.com>
2017-07-19 12:46:41 +0200

    7.1 upiększacz tekstów

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/PoemMain.java
@@ -0,0 +1,13 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
+public class PoemMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println(poemBeautifier.beautify("litwo ojczyzno moja ty jesteś jak zdrowie", poem -> poem.toUpperCase()));
+        System.out.println(poemBeautifier.beautify("litwo ojczyzno moja ty jesteś jak zdrowie", poem -> poem.toLowerCase()));
+        System.out.println(poemBeautifier.beautify("litwo ojczyzno moja ty jesteś jak zdrowie", poem -> "***" + poem + "***"));
+        System.out.println(poemBeautifier.beautify("litwo ojczyzno moja ty jesteś jak zdrowie", poem -> poem.replace("o", "0").replace("e", "3")));
+    }
+}
