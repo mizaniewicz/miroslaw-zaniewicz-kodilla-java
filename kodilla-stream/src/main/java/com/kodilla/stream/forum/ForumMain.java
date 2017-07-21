95b2f879a55735d6e3086c96ca3b342d6bc0f029
mizaniewicz <mizaniewicz@gmail.com>
2017-07-21 11:18:49 +0200

    m7z3 - funkcyjny spacer po liście użytkowników forum

--- kodilla-stream/src/main/java/com/kodilla/stream/forum/ForumMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forum/ForumMain.java
@@ -1,14 +1,16 @@
 package com.kodilla.stream.forum;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ForumMain {
     public static void main(String[] args) {
         Forum forum = new Forum();
-        Map<Integer, Object> mapOfForumUsers = forum.getForumUsersList().stream()
+        Map<Integer, ForumUser> mapOfForumUsers = forum.getForumUsersList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> forumUser.getBirthDate().getYear() < 1997)
+                .filter(forumUser -> Period.between(forumUser.getBirthDate(), LocalDate.now()).getYears() > 20)
                 .filter(forumUser -> forumUser.getPostsAmount() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
 
