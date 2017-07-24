84430dcd98ea8435949ba160ed6c81235e84e0db
mizaniewicz <mizaniewicz@gmail.com>
2017-07-24 16:43:52 +0200

    m7z5 - obliczamy średnią, funkcyjnie

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,21 +1,23 @@
 package com.kodilla.stream.array;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    public static double getAvarage(int[] numbers) {
-        List list = IntStream.range(0, numbers.length)
-                .mapToObj(n -> numbers[n])
-                .collect(Collectors.toList());
+    static double getAvarage(int[] numbers) {
+//        List list = IntStream.range(0, numbers.length)
+//                .mapToObj(n -> numbers[n])
+//                .collect(Collectors.toList());
+
+        Arrays.stream(numbers).forEach(n -> System.out.print(n + " "));
+        System.out.println();
 
         double avarage = IntStream.range(0, numbers.length)
                 .map(n -> numbers[n])
                 .average()
                 .getAsDouble();
 
-        System.out.println("Numbers list: " + list + " Numbers average: " + avarage);
+        System.out.println("Numbers average: " + avarage);
         return avarage;
     }
 }
