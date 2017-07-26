5e42ab6ba5d3b18e6c9b5ef361e16e472aacd862
mizaniewicz <mizaniewicz@gmail.com>
2017-07-26 13:33:11 +0200

    m8z3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Flight.java
@@ -0,0 +1,13 @@
+package com.kodilla.exception.test;
+
+public class Flight {
+    private String departureAirport;
+
+    public Flight(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+}
