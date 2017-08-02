130eafae1006d80d4921d2bc03180d1b254199ad
mizaniewicz <mizaniewicz@gmail.com>
2017-08-02 12:27:26 +0200

    m9z1 - funkcyjna iteracja po tytułach

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreRunner.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreRunner.java
@@ -5,10 +5,10 @@ import java.util.stream.Collectors;
 public class MovieStoreRunner {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
-        String movieList = movieStore.getMovies().entrySet().stream()
+        String moviesList = movieStore.getMovies().entrySet().stream()
                 .flatMap(m -> m.getValue().stream())
                 .collect(Collectors.joining(" ! "));
 
-        System.out.println(movieList);
+        System.out.println(moviesList);
     }
 }
