9356864bbb84f69279927e874f6bbeacb87416ab
mizaniewicz <mizaniewicz@gmail.com>
2017-08-10 13:49:10 +0200

    konfiguracja przy pomocy klasy java

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.library;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,9 +8,14 @@ import java.util.List;
 @Service
 public class Library {
     private final List<String> books = new ArrayList<>();
-
-    @Autowired
     private LibraryDBController libraryDBController;
+
+    public Library(final LibraryDBController libraryDBController) {
+        this.libraryDBController = libraryDBController;
+    }
+
+    public Library() {
+    }
 
     public void saveToDB() {
         libraryDBController.saveData();
