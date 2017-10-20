b7112780e5787843e140ee18168a9cd41d8fb07b
mizaniewicz <mizaniewicz@gmail.com>
2017-10-20 13:40:26 +0200

    programowanie aspektowe w spring boot

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/decorator/pizza/Watcher.java
@@ -0,0 +1,16 @@
+package com.kodilla.patterns2.decorator.pizza;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+public class Watcher {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Watcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.decorator.pizza.*.*(..))")
+    public void logEvent() {
+        LOGGER.info("Decorator added");
+    }
+}
