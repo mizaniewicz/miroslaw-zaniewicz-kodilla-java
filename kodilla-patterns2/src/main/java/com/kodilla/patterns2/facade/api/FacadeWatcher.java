5f9fecd5fb5a470860764b4f54aec626256c021e
mizaniewicz <mizaniewicz@gmail.com>
2017-10-30 11:30:12 +0100

    m20z3 - dodajemy logowanie żądań do fasady

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/FacadeWatcher.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class FacadeWatcher {
+    public static final Logger LOGGER = LoggerFactory.getLogger(FacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" + "&& target(object)")
+    public void logEvent(Object object) {
+        LOGGER.info("Processing order: " + object.getClass().getName());
+    }
+}
